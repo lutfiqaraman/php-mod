fa573e56e017fc5cbbd3d27a458de0569f2ed299
lutfiqaraman <lutfiqaraman@gmail.com>
2016-03-04 21:29:56 +1300

    Update the comment

--- number.php
+++ number.php
@@ -7,6 +7,12 @@
 
 $sum = 0;
 
+/*
+ * The loop to check echo number from 0 to 1000
+ * if the number is multiples of 3 or 5
+ * The remainder of $number divided by 3 or 5 should be 0
+ */
+
 for ($number = 0; $number < 1000; $number++) {
     if (($number % 3 == 0) || ($number % 5 == 0)) {
         $sum += $number;
